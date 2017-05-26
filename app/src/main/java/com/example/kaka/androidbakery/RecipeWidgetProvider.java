114c83014843a71f7e5ec51dfcf212aa8fe789a1
Shrikanth Malarouth <mvnshrikanth@gmail.com>
2017-05-25 23:18:49 -0400

    Creating app widget

--- app/src/main/java/com/example/kaka/androidbakery/RecipeWidgetProvider.java
+++ app/src/main/java/com/example/kaka/androidbakery/RecipeWidgetProvider.java
@@ -1,9 +1,13 @@
 package com.example.kaka.androidbakery;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.example.kaka.androidbakery.ui.MainActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -13,10 +17,11 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget_provider);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.iv_recipe_widget, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
